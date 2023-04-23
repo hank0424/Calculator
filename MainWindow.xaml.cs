6e7c89ff328e17059a2f7c9922df57d56bb1eb62
hank0424 <125251101+hank0424@users.noreply.github.com>
2023-04-24 00:35:26 +0800

    基本四則運算
    新增:
    加、減、乘、除、等於、 小數點、        消除鍵、第二數字儲存

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,16 +20,84 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        float firstNumber, secondNumber; 
+        int operators = -1;
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text); 
+            txtNumber.Text = "0"; 
+            operators = 0; 
+        }
 
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 1; 
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 2; 
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 3; 
+        }
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f; 
+            secondNumber = Convert.ToSingle(txtNumber.Text); 
+
+            
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); 
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = "0";
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
         private void btnZero_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
+                txtNumber.Text = " ";
                 txtNumber.Text += "0";
             }
             
@@ -38,81 +106,82 @@ namespace Calculator
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text +="1";
+                txtNumber.Text = " ";               
             }
+            txtNumber.Text += "1";
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "2";
+                txtNumber.Text = "";              
             }
+            txtNumber.Text += "2";
         }
 
         private void btnThree_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "3";
+                txtNumber.Text = "";               
             }
+            txtNumber.Text += "3";
         }
 
         private void btnFour_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "4";
+                txtNumber.Text = "";              
             }
+            txtNumber.Text += "4";
         }
 
         private void btnFive_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "5";
+                txtNumber.Text = "";             
             }
+            txtNumber.Text += "5";
         }
 
         private void btnSix_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "6";
+                txtNumber.Text = "";              
             }
+            txtNumber.Text += "6";
         }
 
         private void btnSeven_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "7";
+                txtNumber.Text = "";             
             }
+            txtNumber.Text += "7";
         }
 
         private void btnEight_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "8";
+                txtNumber.Text = "";               
             }
+            txtNumber.Text += "8";
         }
+
 
         private void btnNime_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
-                txtNumber.Text = "";
-                txtNumber.Text += "9";
+                txtNumber.Text = "";              
             }
+            txtNumber.Text += "9";
         }
     }
 }
